67c89e8a5b981bf73e4e2541088f99e6264f3c04
Anderson Garcia <andersongarcia@gmail.com>
2017-11-08 03:31:05 -0200

    notificação de administração do remédio para o cuidador

--- functions/index.js
+++ functions/index.js
@@ -38,7 +38,7 @@ exports.pushNotification = functions.database.ref('/messages/{pushId}').onWrite(
         },
     };
 
-    //Create an options object that contains the time to live for the notification and the priority
+    // Cria um objeto de opções que contém o tempo em que a notificação ficará disponível e sua prioridade
     const options = {
         priority: "high",
         timeToLive: 60 * 60 * 24
@@ -48,27 +48,119 @@ exports.pushNotification = functions.database.ref('/messages/{pushId}').onWrite(
     return admin.messaging().sendToTopic("pushNotifications", payload, options);
 });
 
+// Envia nova mensagem para destinarário, com link para áudio
 exports.pushNovaMensagem = functions.database.ref('/mensagens/{idosoId}/{mensagemId}').onWrite( event => {
     
     console.log('Push new message event triggered');
 
     const mensagem = event.data.val();
 
-    // Create a data payload
+    // Cria um payload de dados
     const payload = {
         data: {
+            label: 'mensagem',  // identifica destino da mensagem
             id: mensagem.id, 
             destinatarioId: mensagem.destinatarioId,
             emissorId: mensagem.emissorId,
-            fileName: mensagem.fileName
+            fotoUri: mensagem.fotoUri,
+            audioUri: mensagem.audioUri
         },
     };
     
-    //Create an options object that contains the time to live for the notification and the priority
+    // Cria um objeto de opções que contém o tempo em que a notificação ficará disponível e sua prioridade
     const options = {
         priority: "high",
         timeToLive: 60 * 60 * 24
     };
 
     return admin.messaging().sendToTopic(mensagem.destinatarioId, payload, options);
+});
+
+// Envia solicitação de sincronização dos remédios 
+exports.pushSyncRemedios = functions.database.ref('/remedios/{idosoId}').onWrite( event => {
+
+    console.log('Push sync Remedios event triggered');
+    
+    const idosoId = event.params.idosoId;
+
+    // Cria um payload de dados
+    const payload = {
+        data: {
+            label: 'remedios',   // identifica destino da mensagem
+        },
+    };
+    
+    // Cria um objeto de opções que contém o tempo em que a notificação ficará disponível e sua prioridade
+    const options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+    return admin.messaging().sendToTopic(idosoId, payload, options);
+});
+
+
+// Envia solicitação de sincronização dos remédios 
+exports.pushSyncProgramas = functions.database.ref('/programas/{idosoId}').onWrite( event => {
+    
+    console.log('Push sync Programas event triggered');
+    
+    const idosoId = event.params.idosoId;
+
+    // Cria um payload de dados
+    const payload = {
+        data: {
+            label: 'programas',   // identifica destino da mensagem
+        },
+    };
+    
+    // Cria um objeto de opções que contém o tempo em que a notificação ficará disponível e sua prioridade
+    const options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+    return admin.messaging().sendToTopic(idosoId, payload, options);
+});
+
+// Envia notificação de administração do remédio para o cuidador
+exports.pushNotificaCuidadorRemedio = functions.database.ref('/alertaRemedio/{idosoId}/{remedioId}').onWrite( event => {
+
+    console.log('Push send notification CuidadorRemedio event triggered');
+    
+    const idosoId = event.params.idosoId;
+    const remedioId = event.params.remedioId;
+
+    // Cria um objeto de opções que contém o tempo em que a notificação ficará disponível e sua prioridade
+    const options = {
+        priority: "high",
+        timeToLive: 60 * 60 * 24
+    };
+
+    admin.database().ref('remedios/' + idosoId + '/' + remedioId).once('value', (snapshot) => {
+        var remedio = snapshot.val();
+        console.log('Remédio: ' + remedio);
+        admin.database().ref('contatos/' + idosoId).once('value', (idosoSnapshot) => {
+            var idoso = idosoSnapshot.val();
+            admin.database().ref('idosos/' + idosoId + '/cuidadores').once('value', (cuidadoresSnapshot) => {
+                var cuidadores = [];
+                cuidadoresSnapshot.forEach(function(child) {
+                    console.log(child);
+                    cuidadores.push("'" + child.key + "' in topics");
+                });
+                var condition = cuidadores.join(" || ");
+
+                // Cria um payload de dados
+                const payload = {
+                    data: {
+                        label: 'alertaRemedio',   // identifica destino da mensagem
+                        remedio: remedio,
+                        idoso: idoso
+                    },
+                };
+
+                return admin.messaging().sendToCondition(condition, payload, options);
+            });
+        });
+    });
 });
